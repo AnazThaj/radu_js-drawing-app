4000ab9b43457018cd8d415a16ce6fb548e4b63b
Thaj Anaz <anaz.thaj@gmail.com>
2023-02-05 12:33:52 +0530

    Create undo button

--- web/js/sketchPad.js
+++ web/js/sketchPad.js
@@ -10,6 +10,13 @@ class SketchPad {
     `;
     container.appendChild(this.canvas);
 
+    const lineBreak = document.createElement("br");
+    container.appendChild(lineBreak);
+
+    this.undoBtn = document.createElement("button");
+    this.undoBtn.innerHTML = "UNDO";
+    container.appendChild(this.undoBtn);
+
     this.ctx = this.canvas.getContext("2d");
 
     this.paths = [];
@@ -56,11 +63,22 @@ class SketchPad {
     this.canvas.ontouchend = (evt) => {
       this.canvas.onmouseup();
     };
+
+    this.undoBtn.onclick = () => {
+      this.paths.pop();
+      this.#redraw();
+    };
   }
 
   #redraw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     draw.paths(this.ctx, this.paths);
+
+    if (this.paths.length > 0) {
+      this.undoBtn.disabled = false;
+    } else {
+      this.undoBtn.disabled = true;
+    }
   }
 
   #getMouse = (evt) => {
