7d198712d2dfba1aaaeec72466e3b134e44c01aa
Thaj Anaz <anaz.thaj@gmail.com>
2023-02-19 04:21:30 +0530

    Complete lesson 3

--- web/js/display.js
+++ web/js/display.js
@@ -13,7 +13,7 @@ function createRow(container, studentName, samples) {
 
     const sampleContainer = document.createElement("div");
     sampleContainer.id = "sample_" + id;
-    sampleContainer.onclick = () => handleClick(sample);
+    sampleContainer.onclick = () => handleClick(sample, false);
     sampleContainer.classList.add("sampleContainer");
 
     const sampleLabel = document.createElement("div");
@@ -28,14 +28,33 @@ function createRow(container, studentName, samples) {
   }
 }
 
-function handleClick(sample) {
+function handleClick(sample, doScroll = true) {
+  if (sample == null) {
+    [...document.querySelectorAll(".emphasize")].forEach((e) =>
+      e.classList.remove("emphasize")
+    );
+    return;
+  }
+
+  const el = document.getElementById("sample_" + sample.id);
+
+  if (el.classList.contains("emphasize")) {
+    el.classList.remove("emphasize");
+    chart.selectSample(null);
+    return;
+  }
+
   [...document.querySelectorAll(".emphasize")].forEach((e) =>
     e.classList.remove("emphasize")
   );
-  const el = document.getElementById("sample_" + sample.id);
   el.classList.add("emphasize");
-  el.scrollIntoView({
-    behavior: "auto",
-    block: "center",
-  });
+
+  if (doScroll) {
+    el.scrollIntoView({
+      behavior: "auto",
+      block: "center",
+    });
+  }
+
+  chart.selectSample(sample);
 }
